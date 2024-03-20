0fd3d358f43d048819bb22f362fa0517537a4b2e
Tasawar Abbas <taswarabbassss@gmail.com>
2024-03-21 00:50:32 +0500

    Completed GlobalExceptionHandler With ApiResponse

--- UserService/src/main/java/com/lcwd/user/service/UserService/exeptions/GlobalExceptionHandler.java
+++ UserService/src/main/java/com/lcwd/user/service/UserService/exeptions/GlobalExceptionHandler.java
@@ -2,15 +2,19 @@ package com.lcwd.user.service.UserService.exeptions;
 
 import com.lcwd.user.service.UserService.exceptions.ResourceNotFoundException;
 import com.lcwd.user.service.UserService.payloads.ApiResponse;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    public ResponseEntity<ApiResponse> ResourceNotFoundExceptionHandler(ResourceNotFoundException exception){
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ApiResponse> ResourceNotFoundExceptionHandler(@NotNull ResourceNotFoundException exception) {
         String message = exception.getMessage();
-
+        ApiResponse response = ApiResponse.builder().message(message).success(true).status(HttpStatus.NOT_FOUND).build();
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
     }
-}
+};;
