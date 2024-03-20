87156e2e849970258e6f7bd6eb24623a6de51473
Tasawar Abbas <taswarabbassss@gmail.com>
2024-03-20 23:50:09 +0500

    added user entity

---
+++ UserService/src/main/java/com/lcwd/user/service/UserService/entities/User.java
@@ -0,0 +1,21 @@
+package com.lcwd.user.service.UserService.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "micro_users")
+public class User {
+
+    @Id
+    @Column(name = "ID")
+    private String userId;
+    @Column(name="NAME")
+    private String name;
+    @Column(name = "EMAIL")
+    private String email;
+    @Column(name = "ABOUT")
+    private String about;
+}
