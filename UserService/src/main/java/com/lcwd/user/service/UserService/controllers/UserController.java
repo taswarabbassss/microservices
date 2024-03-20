0ebc1208e05d2cd897138ffeb4d00f46ff7034f0
Tasawar Abbas <taswarabbassss@gmail.com>
2024-03-21 01:27:58 +0500

    Implementation

--- UserService/src/main/java/com/lcwd/user/service/UserService/controllers/UserController.java
+++ UserService/src/main/java/com/lcwd/user/service/UserService/controllers/UserController.java
@@ -5,10 +5,7 @@ import com.lcwd.user.service.UserService.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,14 +16,14 @@ public class UserController {
     private UserService userService;
 
     @PostMapping
-    public ResponseEntity<User> saveUser(User user) {
+    public ResponseEntity<User> saveUser(@RequestBody User user) {
         User user1 = userService.saveUser(user);
         return ResponseEntity.status(HttpStatus.CREATED).body(user1);
     }
 
 
     @GetMapping("/{userId}")
-    public ResponseEntity<User> getSingleUser(String userId) {
+    public ResponseEntity<User> getSingleUser(@PathVariable("userId") String userId) {
         User userById = userService.getUserById(userId);
         return ResponseEntity.ok(userById);
     }
