2f964fd272e3fe7aded8bd984867056463e30e61
Tasawar Abbas <taswarabbassss@gmail.com>
2024-03-21 20:53:29 +0500

    Created Hotel Entity

--- HotelService/src/main/java/com/lcwd/hotel/HotelServiceApplication.java
+++ HotelService/src/main/java/com/lcwd/hotel/HotelServiceApplication.java
@@ -8,6 +8,7 @@ public class HotelServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HotelServiceApplication.class, args);
+		System.out.println("App is running successfully...");
 	}
 
 }
