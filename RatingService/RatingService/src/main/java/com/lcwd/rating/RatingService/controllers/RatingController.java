ac091dbb6c9eff7d02fd952764cf6ddf82b208b7
Tasawar Abbas <taswarabbassss@gmail.com>
2024-03-22 00:25:06 +0500

    Implemented Rating Controller

---
+++ RatingService/RatingService/src/main/java/com/lcwd/rating/RatingService/controllers/RatingController.java
@@ -0,0 +1,37 @@
+package com.lcwd.rating.RatingService.controllers;
+
+import com.lcwd.rating.RatingService.entities.Rating;
+import com.lcwd.rating.RatingService.services.RatingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/ratings")
+public class RatingController {
+
+    @Autowired
+    private RatingService ratingService;
+
+    @GetMapping
+    public ResponseEntity<List<Rating>> getAllUsers() {
+        return ResponseEntity.ok(ratingService.getAllRating());
+    }
+    @PostMapping
+    public ResponseEntity<Rating> createNewRating(@RequestBody Rating rating){
+        return ResponseEntity.status(HttpStatus.CREATED).body(ratingService.createRating(rating));
+    }
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<List<Rating>> getRatingByUserId(@PathVariable String userId){
+        return ResponseEntity.ok(ratingService.getAllRatingByUserId(userId));
+    }
+
+    @GetMapping("/{hotelId}")
+    public ResponseEntity<List<Rating>> getRatingByHotelId(@PathVariable String hotelId){
+        return ResponseEntity.ok(ratingService.getAllRatingByHotelId(hotelId));
+    }
+}
