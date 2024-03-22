3519eacb0e0b78242eb6e7a757864763818e1214
Tasawar Abbas <taswarabbassss@gmail.com>
2024-03-22 20:27:36 +0500

    made the project Registry Service

--- ServiceRegistory/src/main/java/com/lcwd/registory/ServiceRegistory/ServiceRegistoryApplication.java
+++ ServiceRegistory/src/main/java/com/lcwd/registory/ServiceRegistory/ServiceRegistoryApplication.java
@@ -2,8 +2,10 @@ package com.lcwd.registory.ServiceRegistory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class ServiceRegistoryApplication {
 
 	public static void main(String[] args) {
