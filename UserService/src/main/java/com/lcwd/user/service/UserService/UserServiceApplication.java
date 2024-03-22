fdf0046da1058e1169428a190f4edbe5206221d1
Tasawar Abbas <taswarabbassss@gmail.com>
2024-03-22 21:07:44 +0500

    Complete Setup to implement Eureka Client into a Microservice

--- UserService/src/main/java/com/lcwd/user/service/UserService/UserServiceApplication.java
+++ UserService/src/main/java/com/lcwd/user/service/UserService/UserServiceApplication.java
@@ -2,10 +2,11 @@ package com.lcwd.user.service.UserService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class UserServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(UserServiceApplication.class, args);
 		System.out.println("User App is running successfully...");
