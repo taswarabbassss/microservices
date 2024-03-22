e320aa9e085bb3c637e3b5e86dc01bbd45e48605
Tasawar Abbas <taswarabbassss@gmail.com>
2024-03-22 20:26:06 +0500

    Service Registry base code

---
+++ ServiceRegistory/src/main/java/com/lcwd/registory/ServiceRegistory/ServiceRegistoryApplication.java
@@ -0,0 +1,14 @@
+package com.lcwd.registory.ServiceRegistory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceRegistoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceRegistoryApplication.class, args);
+		System.out.println("Service Registry App is running Successfully...");
+	}
+
+}
